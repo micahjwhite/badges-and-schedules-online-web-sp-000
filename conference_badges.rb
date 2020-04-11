d2828cbf7a65db7b87109bf8153dadceeecd24f9
Micah White <micahjwhite@gmail.com>
2020-04-11 02:30:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
   new_array = []
   array.each do |name| 
-    return "Hello, my name is #{name}."
+    new_array.push("Hello, my name is #{name}.")
   end
 end
